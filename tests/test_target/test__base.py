cf488341849400a57688e9c3d8455ae1260e7f3b
Anders Huss <5502349+andhus@users.noreply.github.com>
2025-02-09 23:21:43 +0100

    adds proxy_path to filesystem targets

--- tests/test_target/test__base.py
+++ tests/test_target/test__base.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from stardag.target import LocalTarget, MockRemoteFileSystem, RemoteFileSystemTarget
+from stardag.target import InMemoryRemoteFileSystem, LocalTarget, RemoteFileSystemTarget
 
 
 def test_local_target(tmp_path: Path):
@@ -37,8 +37,26 @@ def test_local_target_binary(tmp_path: Path):
         assert f.read() == b"test"
 
 
+def test_local_target_proxy_path(tmp_path: Path):
+    target = LocalTarget(str(tmp_path / "test.txt"))
+    assert not target.exists()
+
+    with target.proxy_path("w") as proxy_path:
+        with proxy_path.open("w") as f:
+            f.write("test")
+
+    assert target.exists()
+
+    with target.open("r") as proxy_handle:
+        assert proxy_handle.read() == "test"
+
+    with target.proxy_path("r") as proxy_path:
+        with proxy_path.open("r") as proxy_handle:
+            assert proxy_handle.read() == "test"
+
+
 def test_remote_filesystem_target():
-    rfs = MockRemoteFileSystem()
+    rfs = InMemoryRemoteFileSystem()
     uri = "s3://bucket/key"
     target = RemoteFileSystemTarget(path=uri, rfs=rfs)
     assert not target.exists()
@@ -60,7 +78,7 @@ def test_remote_filesystem_target():
 # NOTE we are intentionally not parameterizing this test with binary=True/False
 # to also check type checking based in the mode parameter.
 def test_remote_filesystem_target_binary():
-    rfs = MockRemoteFileSystem()
+    rfs = InMemoryRemoteFileSystem()
     uri = "s3://bucket/key"
     target = RemoteFileSystemTarget(path=uri, rfs=rfs)
     assert not target.exists()
@@ -75,5 +93,27 @@ def test_remote_filesystem_target_binary():
 
     with target.open("rb") as f:
         assert f.read() == b"test"
+
+    assert rfs.uri_to_bytes[uri] == b"test"
+
+
+def test_remote_filesystem_target_proxy_path():
+    rfs = InMemoryRemoteFileSystem()
+    uri = "s3://bucket/key"
+    target = RemoteFileSystemTarget(path=uri, rfs=rfs)
+    assert not target.exists()
+
+    with target.proxy_path("w") as proxy_path:
+        with proxy_path.open("w") as f:
+            f.write("test")
+
+    assert target.exists()
+
+    with target.open("r") as proxy_handle:
+        assert proxy_handle.read() == "test"
+
+    with target.proxy_path("r") as proxy_path:
+        with proxy_path.open("r") as proxy_handle:
+            assert proxy_handle.read() == "test"
 
     assert rfs.uri_to_bytes[uri] == b"test"
