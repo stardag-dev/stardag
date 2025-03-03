3f0931d446b4bba9e5ebcf317bbc9c88986fd219
Anders Huss <5502349+andhus@users.noreply.github.com>
2025-03-03 21:37:03 +0100

    make examples a package

--- tests/test_examples/test_ml_pipeline/test_decorator_api.py
+++ tests/test_examples/test_ml_pipeline/test_decorator_api.py
@@ -1,4 +1,5 @@
 import pytest
+from stardag_examples.ml_pipeline.decorator_api import get_metrics_dag
 
 from stardag.build.sequential import build as build_sequential
 from stardag.target.serialize import JSONSerializer, PandasDataFrameCSVSerializer
@@ -10,17 +11,15 @@ except ImportError:
 
 
 @pytest.mark.skipif(pd is None, reason="pandas is not installed")
-def test_build_metrics_dag(default_in_memory_fs_target, examples_in_sys_path):
-    from ml_pipeline.decorator_api import get_metrics_dag  # type: ignore
-
+def test_build_metrics_dag(default_in_memory_fs_target):
     metrics = get_metrics_dag()
     assert isinstance(metrics._serializer, JSONSerializer)
     assert metrics.output().path.endswith(".json")
     assert metrics.output().path.startswith(
         "in-memory://examples/ml_pipeline/decorator_api/metrics/v0/"
     )
-    assert isinstance(metrics.predictions._serializer, PandasDataFrameCSVSerializer)
-    assert metrics.predictions.output().path.endswith(".csv")
+    assert isinstance(metrics.predictions._serializer, PandasDataFrameCSVSerializer)  # type: ignore
+    assert metrics.predictions.output().path.endswith(".csv")  # type: ignore
     build_sequential(metrics)
     assert metrics.complete()
     assert metrics.output().exists()
