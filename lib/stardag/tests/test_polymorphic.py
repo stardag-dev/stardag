03a412fa14d4a33800887df355b9323990938593
Anders Huss <5502349+andhus@users.noreply.github.com>
2026-01-02 08:18:15 +0100

    improve test

--- lib/stardag/tests/test_polymorphic.py
+++ lib/stardag/tests/test_polymorphic.py
@@ -41,11 +41,22 @@ def test_smoke():
         def extra_info(self) -> int:
             return self.wing_span_cm
 
+    # create a different family
+    class Tool(PolymorphicRoot):
+        pass
+
+    class Hammer(Tool):
+        weight_kg: float
+
+    class Screwdriver(Tool):
+        length_cm: int
+
     class Container(BaseModel):
         strict_item: Dog
         poly_item: SubClass[Animal]
         poly_items: list[SubClass[Animal]]
         bird: SubClass[BirdBase]
+        tool: SubClass[Tool]
 
     dog_data = {
         NAMESPACE_KEY: "",
@@ -65,11 +76,18 @@ def test_smoke():
         "vocabulary_size": 50,
     }
 
+    tool_data = {
+        NAMESPACE_KEY: "",
+        TYPE_KEY: "Hammer",
+        "weight_kg": 2.5,
+    }
+
     container_data = {
         "strict_item": dog_data,
         "poly_item": cat_data,
         "poly_items": [dog_data, cat_data],
         "bird": parrot_data,
+        "tool": tool_data,
     }
 
     container = Container.model_validate(container_data)
@@ -89,18 +107,30 @@ def test_smoke():
     assert isinstance(container.bird, Parrot)
     assert container.bird.vocabulary_size == 50
 
-    registry = Animal._registry()
-    assert Dog._registry() is registry
-    assert Cat._registry() is registry
-    assert BirdBase._registry() is registry
-    assert Parrot._registry() is registry
-    assert Sparrow._registry() is registry
+    assert isinstance(container.tool, Hammer)
+    assert container.tool.weight_kg == 2.5
 
-    expected_type_id_to_class = {
+    animal_registry = Animal._registry()
+    tool_registry = Tool._registry()
+    assert tool_registry is not animal_registry
+
+    assert Dog._registry() is animal_registry
+    assert Cat._registry() is animal_registry
+    assert BirdBase._registry() is animal_registry
+    assert Parrot._registry() is animal_registry
+    assert Sparrow._registry() is animal_registry
+    expected_animal_type_id_to_class = {
         TypeId(namespace="", name="Dog"): Dog,
         TypeId(namespace="", name="Cat"): Cat,
         TypeId(namespace="", name="Parrot"): Parrot,
         TypeId(namespace="", name="Sparrow"): Sparrow,
     }
+    assert animal_registry._type_id_to_class == expected_animal_type_id_to_class
 
-    assert registry._type_id_to_class == expected_type_id_to_class
+    assert Hammer._registry() is tool_registry
+    assert Screwdriver._registry() is tool_registry
+    expected_tool_type_id_to_class = {
+        TypeId(namespace="", name="Hammer"): Hammer,
+        TypeId(namespace="", name="Screwdriver"): Screwdriver,
+    }
+    assert tool_registry._type_id_to_class == expected_tool_type_id_to_class
