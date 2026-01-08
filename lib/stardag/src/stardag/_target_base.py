1cebd79e93de10eab733081a1aecf7428fc0a12a
Anders Huss <5502349+andhus@users.noreply.github.com>
2026-01-08 23:03:00 +0100

    draft aio interface

--- lib/stardag/src/stardag/_target_base.py
+++ lib/stardag/src/stardag/_target_base.py
@@ -5,5 +5,10 @@ from typing import Protocol, TypeVar, runtime_checkable
 class Target(Protocol):
     def exists(self) -> bool: ...
 
+    async def exists_aio(self) -> bool:
+        """Asynchronously check if the target exists."""
+        ...
+        return self.exists()
+
 
 TargetType = TypeVar("TargetType", bound=Target, covariant=True)
