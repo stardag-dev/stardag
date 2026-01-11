4e7f93642b0043a1fd3bd57faff87903f66fbd4b
Anders Huss <5502349+andhus@users.noreply.github.com>
2026-01-11 20:00:44 +0100

    cleanup test structure for build

--- lib/stardag/tests/test_build/conftest.py
+++ lib/stardag/tests/test_build/conftest.py
@@ -7,5 +7,8 @@ from stardag.registry import NoOpRegistry
 
 @pytest.fixture
 def noop_registry():
-    """Provide a NoOpRegistry for tests."""
+    """Provide a NoOpRegistry for tests.
+
+    TODO remove? Could be wrapped by Mock to track called methods.
+    """
     return NoOpRegistry()
